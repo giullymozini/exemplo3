43a98fc40f7abf7243842d93c0fb56abce2cdd30
Giuliana <giulianalucaslima@gmail.com>
2024-08-26 09:05:43 -0300

    criando as rotas dos componentes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Error from './routes/Error.jsx'
+import Aluno from './routes/Aluno.jsx'
+
+
+{/*criando a funçao createbrowserrouter */ }
+
+const router = createBrowserRouter([{
+  //chamando os elementos pai 
+  path: '/', element: <App />,
+  errorElement: <Error />,
+
+  /*chamando os elementos filhos */
+  children: [
+    { path: '/', element: <Home /> },
+    {path: '/aluno', element:<Aluno/>}
+
+  ]
+
+}])
+
+
+
+
+
+
+
+
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/*criando o props q sera renderizado*/}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/routes/Aluno.jsx
@@ -0,0 +1,9 @@
+const Aluno = () => {
+
+    return (
+        <>
+            <h1>Página aluno</h1>
+        </>
+    )
+}
+export default Aluno
